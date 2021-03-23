6f2ba28cdfc2430153b23d4563ccc67ef3e41a4e
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-23 16:45:54 -0400

    Replaced browserRouter with hashRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomePage from 'components/home-page/HomePage';
 import About from 'components/about-us/About';
