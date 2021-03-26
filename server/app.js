3f5b2ec4b2b370f769433321e280b20dc03eba52
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-26 19:52:25 -0400

    Added middlewared & refactored code.

--- server/app.js
+++ server/app.js
@@ -1,15 +1,29 @@
-const express = require('express');
-
-const authenticationRoutes = require('./routes/auth');
-
 require('dotenv').config();
 require('./database/db');
 
-const PORT = process.env.PORT || 7000;
+const express = require('express');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+const authenticationRoutes = require('./routes/auth');
+
+// Express Application
 const app = express();
 
+// Application Middlewares
+app.use(cors());
+app.use(expressValidator());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Application Routing Middlewares
 app.use('/auth', authenticationRoutes);
+
+const PORT = 7000;
 
 app.listen(PORT, () => {
     console.log(`Server started running at PORT : ${PORT}`);
