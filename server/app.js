01adf50938848ca3eeaaa6bbf946a49f03f58ae3
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-26 19:40:47 -0400

    Connected database, setup env, created basic routing.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
+const authenticationRoutes = require('./routes/auth');
+
+require('dotenv').config();
+require('./database/db');
+
+const PORT = process.env.PORT || 7000;
+
 const app = express();
 
-const PORT = 5000;
+app.use('/auth', authenticationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started running at PORT : ${PORT}`);
