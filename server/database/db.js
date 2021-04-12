d322f6f6698dff0187c887bf294ff6625c00c289
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-04-12 12:35:35 -0400

    Excluded node.js server from the project.

--- server/database/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB has been connected!'));
--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const { signUpController } = require('../controllers/auth');
-
-const router = express.Router();
-
-router.post('/sign-up', signUpController);
-
-module.exports = router;
